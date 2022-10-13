5d94da9d8645581f4b49af6f2a93e6fc2d194d50
SYOMITI <115490512+SYOMITI@users.noreply.github.com>
2022-10-13 20:30:05 +0300

    first commit

---
+++ vaiables.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int myNum;// variable declaration
+int mynum2{3};//declaration
+int age, year ,phone; //three variable declaration
+char myName, phone;
+bool present;
+float temp;
+double bankBalance;
+// vaviable initialization
+myNum=2;
+age=25;
+year=2022;
+myName ="kilonzo syomiti";
+phone=741427873;
+present="True";
+bankBalance=2000788.034556;
+temp=24.6;
+//display variable data
+//variable name does not contain spaces
+cout<<mynum2;
+cout<<age=<<"myNum;
+cout<<
+
+cout<<"\n"; // display variable data
+}
